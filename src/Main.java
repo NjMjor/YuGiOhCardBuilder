3a60e214a014991251471ce16a35e95a49f3e042
NjMjor <mladen.blazic@hotmail.com>
2017-05-20 01:01:30 +0200

    Almost bug free

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
  * Created by mlade on 16-May-17.
  */
 
-import org.sikuli.script.*;
-
 public class Main
 {
     public static void main(String args[]) throws Exception
     {
-        Card card = new Card("Exodia the Forbidden One");
+        Card card = new Card("Trap Hole");
 
         //String tcgPath = System.getProperty("user.home") + "\\Desktop\\TCG\\TCGEditor.exe";
         //Process tcg = Runtime.getRuntime().exec(tcgPath);
