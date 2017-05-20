55c28fbb5f2e96b08307960fa3939de0b0c5af76
NjMjor <mladen.blazic@hotmail.com>
2017-05-20 04:32:01 +0200

    Almost bug free

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,43 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 /**
  * Created by mlade on 16-May-17.
  */
 
-public class Main
+public class Main extends Application
 {
-    public static void main(String args[]) throws Exception
+    @FXML
+    private TextField cardName;
+
+    @Override public void start(Stage primaryStage) throws Exception
     {
-        Card card = new Card("Trap Hole");
+        Parent root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
+        primaryStage.setTitle("Yu-Gi-Oh! Card Builder");
+        primaryStage.setScene(new Scene(root, 454, 288));
+        primaryStage.show();
 
-        //String tcgPath = System.getProperty("user.home") + "\\Desktop\\TCG\\TCGEditor.exe";
-        //Process tcg = Runtime.getRuntime().exec(tcgPath);
+    }
 
-
+    public void confirmButtonClicked() throws Exception
+    {
+        Card card = new Card(cardName.getText());
 
         TCG t = new TCG();
         t.generateCard(card);
+    }
+
+    public static void main (String args[]) throws Exception
+    {
+        launch(args);
+
+        //String tcgPath = System.getProperty("user.home") + "\\Desktop\\TCG\\TCGEditor.exe";
+        //Process tcg = Runtime.getRuntime().exec(tcgPath);
     }
 }
